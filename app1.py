eb2c38853b2f35449f4008f2c18de0649106408d
jtaylor6 <jarrede20@gmail.com>
2020-12-06 19:02:40 -0500

    made the dictionary accept all letter casing

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word does not exist. Please double check it"
 
 word = input("Enter word: ")
 
